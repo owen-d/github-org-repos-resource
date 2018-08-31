1349635c576c4a2ac455308994e6d0fb0a6229ef
Owen Diehl <owen@fugue.co>
2018-08-31 17:49:27 -0400

    reads from stdin and fetches repos, hashes them to a version

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,16 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Char8 as C8
 
 main :: IO ()
+-- main = Handlers.getRepos conf >>= Handlers.printRepos
 main = do
   json <- B.getContents
   let decoded = A.eitherDecode json :: Either String Config.Input
-  print decoded
+  case decoded of
+    Left err -> putStrLn err
+    Right conf -> do
+      resp <- Handlers.getRepos (Config.source conf)
+      Handlers.printRepos resp
+
+
+-- [Show e => a -> IO (either e b)]
+-- want fn that invokes on
