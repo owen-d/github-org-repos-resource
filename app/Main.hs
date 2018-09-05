c7cd2dd3e9c992a5e6a57b7cc40ecdcfb747310b
Owen Diehl <owen@fugue.co>
2018-09-04 22:20:09 -0400

    propResults{,M} for different signatures

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,10 @@ import qualified Config                as Config
 import qualified Data.Aeson            as A
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy  as B
+import           Data.Maybe            (maybe)
 import qualified GitHub.Auth           as Auth
 import qualified Handlers              as Handlers
 import qualified IOUtils               as IOUtils
-import Data.Maybe (maybe)
 
 -- main = do
 --   json <- B.getContents
@@ -23,24 +23,11 @@ import Data.Maybe (maybe)
 main = do
   json <- B.getContents
   let decoded = A.eitherDecode json :: Either String Config.Input
-  let conf =
-        case decoded of
-          Left err -> do
-            putStrLn err
-            return Nothing
-          Right conf -> return $ Just conf
+  conf <-
+    case decoded of
+      Left err -> do
+        putStrLn err
+        return Nothing
+      Right conf -> return $ Just conf
   repos <- IOUtils.propResults (\x -> Handlers.getRepos (Config.source x)) conf
   maybe (return ()) Handlers.showRepos repos
-
--- main = do
---   json <- B.getContents
---   let decoded = A.eitherDecode json :: Either String Config.Input
---   repos <- case decoded of
---     Left err -> do
---       putStrLn err
---       return Nothing
---     Right conf ->
---       IOUtils.propResults
---         (\x -> Handlers.getRepos (Config.source x))
---         (return $ Just conf)
---   maybe (return ()) Handlers.showRepos repos
--- src/IOUtils.hs
+++ src/IOUtils.hs
@@ -2,8 +2,13 @@ module IOUtils
     ( module IOUtils
     ) where
 
-propResults :: (Show a, Show b) => (a -> IO (Either b c)) -> IO (Maybe a) -> IO (Maybe c)
-propResults fn x = x >>= maybe (return Nothing) (unwrapErr . fn)
+propResultsM ::
+     (Show a, Show b) => (a -> IO (Either b c)) -> IO (Maybe a) -> IO (Maybe c)
+propResultsM fn x = x >>= maybe (return Nothing) (unwrapErr . fn)
+
+
+propResults :: (Show a, Show b) => (a -> IO (Either b c)) -> Maybe a -> IO (Maybe c)
+propResults fn x = maybe (return Nothing) (unwrapErr . fn) x
 
 unwrapErr :: Show a => IO (Either a b) -> IO (Maybe b)
 unwrapErr x = do
